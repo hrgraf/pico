cc535024ce6b49a3ad0cb1b46f6a675a4793953e
H.R.Graf <hrgraf@gmx.ch>
2021-11-09 11:37:16 +0100

    added documentation to oled driver

--- micropython/sh1107.py
+++ micropython/sh1107.py
@@ -1,3 +1,11 @@
+# Micropython driver for SH1107-based OLED display (64 x 128)
+#
+# Driver with the same API as micropython built-in SSD1306 driver:
+# - I2C only
+# - Tested with https://www.aliexpress.com/item/4000464960639.html; both in 128 x 64 and 64 x 128 pixels mode
+# - Not tested in 128 x 128 pixel mode
+# - Implements simple optimization to reduce the amount of data sent during show()
+#
 # https://github.com/nemart69/sh1107-micropython
 # https://www.displayfuture.com/Display/datasheet/controller/SH1107.pdf
 
